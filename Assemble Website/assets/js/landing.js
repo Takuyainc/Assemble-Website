87eb7e2936c42783a0ab97cdde194af117c21b78
Kizzle <k.mergener@assembleteam.com>
2020-07-21 08:50:43 +0200

    Created all files

---
+++ Assemble Website/assets/js/landing.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  var hiddenGames = $(".hiddenbyDefault");
+  $("#show-more-games-button").on("click", function(event){
+    hiddenGames.show();
+    $("#button-holder").remove();
+  });
+
+  $(".game-box").click(function() {
+    window.location = $(this).find("a").attr("href");
+    return false;
+  });
+
+  $("#button-nav").click(function () {
+      $('body').toggleClass('overflow-add');
+      $("#fast-search-wrapper").slideToggle();
+
+  });
+
+});
