bb500a90f2d4e360438e82dd8cb5192c808a9287
Kizzle <k.mergener@assembleteam.com>
2020-07-21 09:31:18 +0200

    Changed something

--- Assemble Website/assets/js/landing.js
+++ Assemble Website/assets/js/landing.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
   $("#button-nav").click(function () {
       $('body').toggleClass('overflow-add');
       $("#fast-search-wrapper").slideToggle();
+      $(".button-nav-top").css({transform: 'translateY(300px) rotateZ(120deg)'})
 
   });
 
